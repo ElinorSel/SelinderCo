0abbfbf49da5155df63e6e2ece6bf13dd3bc003a
ElinorSel <124194603+ElinorSel@users.noreply.github.com>
2023-09-12 15:17:46 +0000

    Add HTML & CSS

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #create an instance of flask "app" with the name being this filename
 app = Flask(__name__)
 
 
-@app.route('/') #define the url for this page / will give us just the default home page
+@app.route(
+    '/'
+)  #define the url for this page / will give us just the default home page
 def helloWorld():
-   return"Hello world"
+  return render_template("home.html")  #renderar den sidan du vill
 
-helloWorld()
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug = True)
+  app.run(
+      host="0.0.0.0", debug=True
+  )  #runnar locally skriver detta bara eftersom flask har en annan run command än default här i replit
