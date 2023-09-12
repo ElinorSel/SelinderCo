e981f4dd169cbf09fb07a942b89911fe2d6b4a36
ElinorSel <124194603+ElinorSel@users.noreply.github.com>
2023-09-12 16:33:24 +0000

    Added dynamic data, api-data, Bootstrap

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 #create an instance of flask "app" with the name being this filename
 app = Flask(__name__)
+
+JOBS = [{
+    "id": 1,
+    "title": "Senior Software Engineer",
+    "location": "Stockholm, Sweden",
+    "salary": "400 000kr"
+}, {
+    "id": 2,
+    "title": "Marketing Manager",
+    "location": "Seoul, South Korea",
+    "salary": "300 000kr"
+}, {
+    "id": 3,
+    "title": "Intern",
+    "location": "Remote"
+}]
 
 
 @app.route(
     '/'
 )  #define the url for this page / will give us just the default home page
 def helloWorld():
-  return render_template("home.html")  #renderar den sidan du vill
+  return render_template("home.html", jobs=JOBS)  #renderar den sidan du vill
+
+
+@app.route('/api/jobs')
+def listJobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
