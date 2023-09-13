6077ff312113e35ca7e15f57d446892cbdbf6400
ElinorSel <124194603+ElinorSel@users.noreply.github.com>
2023-09-13 06:03:48 +0000

    Added Connection to DB
    MySQL
    PlanetScale

--- app.py
+++ app.py
@@ -1,35 +1,22 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 #create an instance of flask "app" with the name being this filename
 app = Flask(__name__)
-
-JOBS = [{
-    "id": 1,
-    "title": "Senior Software Engineer",
-    "location": "Stockholm, Sweden",
-    "salary": "400 000kr"
-}, {
-    "id": 2,
-    "title": "Marketing Manager",
-    "location": "Seoul, South Korea",
-    "salary": "300 000kr"
-}, {
-    "id": 3,
-    "title": "Intern",
-    "location": "Remote"
-}]
 
 
 @app.route(
     '/'
 )  #define the url for this page / will give us just the default home page
 def helloWorld():
-  return render_template("home.html", jobs=JOBS)  #renderar den sidan du vill
+  jobs = load_jobs_from_db()
+  return render_template("home.html", jobs=jobs)  #renderar den sidan du vill
 
 
 @app.route('/api/jobs')
 def listJobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 if __name__ == "__main__":
